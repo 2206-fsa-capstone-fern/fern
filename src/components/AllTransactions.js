cb480b44f881e57e2c9cdfd07b9a151f3e0e74b7
alexjcwong <alex.jc.wong@gmail.com>
2022-08-25 16:34:23 -0400

    transaction table, no dropdown

--- src/components/AllTransactions.js
+++ src/components/AllTransactions.js
@@ -1,18 +1,74 @@
 // monthly expenses as pie chart
 
-import React from "react";
+import React, { useState, useEffect } from "react";
 
 function AllTransactions() {
+  const base = "https://sandbox.plaid.com/";
+  const baseURL = `${base}transactions/get`;
+  const proxyURL = "https://cors-anywhere.herokuapp.com/";
+  const apiKey = process.env.REACT_APP_PLAID_API_Key;
+  const currentDate = new Date();
+  const formattedCurrDate = currentDate.toISOString().split("T")[0];
+  const currentYear = currentDate.getFullYear();
+  const currentMonth = currentDate.getMonth() + 1;
+
+  const [chart, setChart] = useState([]);
+  const [loading, setLoading] = useState(false);
+  // const [currentPage, setCurrentPage] = useState(1);
+  // const [postsPerPage, setPostsPerPage] = useState(50);
+
+  useEffect(() => {
+    const fetchTransactions = async () => {
+      setLoading(true);
+      await fetch(`${proxyURL}${baseURL}`, {
+        method: "POST",
+        headers: {
+          "Content-Type": "application/json",
+          "x-requested-with": `${apiKey}`,
+          "Access-Control-Allow-Origin": "*",
+        },
+        body: JSON.stringify({
+          client_id: process.env.REACT_APP_PLAID_CLIENT_ID,
+          secret: process.env.REACT_APP_PLAID_SECRET,
+          access_token: process.env.REACT_APP_PLAID_ACCESS_TOKEN,
+          start_date: `${currentYear}-${
+            currentMonth < 10 ? `0${currentMonth}` : currentMonth
+          }-01`,
+          end_date: formattedCurrDate,
+        }),
+      })
+        .then((response) => {
+          response.json().then((data) => {
+            setChart(data.transactions);
+            setLoading(false);
+          });
+        })
+
+        .catch((error) => {
+          console.log("ERROR: \n", error);
+        });
+    };
+    fetchTransactions();
+  }, []);
+
   return (
     <div className="accounts">
-      <div className="transactions-bar">
-        <div>Date</div>
-        <div>Description</div>
-        <div>Category</div>
-        <div>Amount</div>
-        {/* this view totally depends on how it comes through from the api */}
-        <div></div>
-      </div>
+      <table className="year">
+        <thead>
+          <th>Date </th>
+          <th>Description</th>
+          <th>Category</th>
+          <th>Amount</th>
+        </thead>
+        {chart.map((account) => (
+          <tbody key={account.transaction_id}>
+            <td>{account.date}</td>
+            <td>{account.name}</td>
+            <td>{account.category[0]}</td>
+            <td>${account.amount.toFixed(2)}</td>
+          </tbody>
+        ))}
+      </table>
     </div>
   );
 }
