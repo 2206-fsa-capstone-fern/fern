94a5d559829a471d7879433225d54aa420d2d8b6
alexjcwong <105082697+alexjcwong@users.noreply.github.com>
2022-08-30 13:05:07 -0400

    Revert "Dashboard dnd"

---
+++ src/components/AllTransactions.js
@@ -0,0 +1,20 @@
+// monthly expenses as pie chart
+
+import React from "react";
+
+function AllTransactions() {
+  return (
+    <div className="accounts">
+      <div className="transactions-bar">
+        <div>Date</div>
+        <div>Description</div>
+        <div>Category</div>
+        <div>Amount</div>
+        {/* this view totally depends on how it comes through from the api */}
+        <div></div>
+      </div>
+    </div>
+  );
+}
+
+export default AllTransactions;
