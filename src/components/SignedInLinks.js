6344ad09b567e7526a9fb494a3bfa38c2a83f884
alexjcwong <alex.jc.wong@gmail.com>
2022-09-07 18:06:51 -0400

    changed back to original sidebar styling with icon change

--- src/components/SignedInLinks.js
+++ src/components/SignedInLinks.js
@@ -12,24 +12,30 @@ const SignedInLinks = (props) => {
     props.logout(navigate);
   };
   return (
-    <div className="navbar-container vstack">
+    <div className="navbar-container">
       {/* <div className="plaidButton">
         <button onClick={() => props.open()} disabled={!props.ready}>
           <strong>Link an account</strong>
         </button>
       </div> */}
-      {isLoggedIn ? (
-        <Button
-          color="success"
-          style={{
-            display: "flex",
-            justifyContent: "space-between",
-            alignItems: "center",
-            marginTop: "400px",
-            width: "90px",
-          }}
+      <Button
+        color="success"
+        style={{ marginRight: "20px", marginTop: "10px" }}
+      >
+        <NavLink
+          to="/"
+          onClick={handleLogout}
+          className="authLink"
+          style={{ color: "white" }}
         >
+          Log Out
+        </NavLink>
+      </Button>
+
+      {isLoggedIn ? (
+        <Button color="success" style={{ marginTop: "10px" }}>
           <MDBIcon fas icon="user" />
+          {"    "}
           <NavLink to="/account" className="authLink">
             {user.firstName[0].toUpperCase()}
             {user.lastName[0].toUpperCase()}
@@ -42,20 +48,6 @@ const SignedInLinks = (props) => {
           </NavLink>
         </Button>
       )}
-
-      <Button
-        color="success"
-        style={{ marginRight: "5px", marginTop: "350px", width: "90px" }}
-      >
-        <NavLink
-          to="/"
-          onClick={handleLogout}
-          className="authLink"
-          style={{ color: "white" }}
-        >
-          Log Out
-        </NavLink>
-      </Button>
     </div>
   );
 };
